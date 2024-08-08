5a528452d96ee8ebc2bda96ac0db4e3879b44ac0
theflyoccultist <y.delahaije@gmail.com>
2024-08-08 02:14:38 +0200

    jwt login

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -30,6 +30,27 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = "brat"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return(
+            jsonify({"message": "the token has expired", "error": "token_expired"}),
+           401,
+        )
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return(
+            jsonify({"message": "signature verification failed", "error": "invalid_token"}),
+           401,
+        )
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return(
+            jsonify({"message": "request does not contain an access token", "error": "authorization_required"}),
+           401,
+        )
 
     with app.app_context():
         db.create_all()
